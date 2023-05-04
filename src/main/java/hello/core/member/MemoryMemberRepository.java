294cb5d2d54b666e44d525b8215b189a0a97801d
isevou__ <kbry12@naver.com>
2023-05-04 18:09:33 +0900

    feature : 회원 등록, 조회

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    // 원래 ConcurrentHashMap 사용해야함.. 동시성 이슈가 발생할 수도 있음
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findbyId(Long memberId) {
+        return store.get(memberId);
+    }
+}
