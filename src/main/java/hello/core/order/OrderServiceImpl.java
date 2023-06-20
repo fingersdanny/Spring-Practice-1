5a0292dd060434e98613226c4be3f9623dab92f5
isevou__ <kbry12@naver.com>
2023-06-20 13:06:30 +0900

    add : OrderServiceImpl 필드 주입 주석 추가

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,14 +12,34 @@ public class OrderServiceImpl implements OrderService{
 
     //불변하는 객체를 바꾸는 Method 조차 만들지 말아야한다.
     //처음부터 불변으로 해놔야지 아니면 나중에 수정하기 힘들어진다.
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    // 스프링 라이프사이클
+    // 1. 스프링 빈 생성
+    // 2. 의존관계 주입
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        System.out.println("memberRepository = " + memberRepository);
+        this.memberRepository = memberRepository;
+    }
 
     @Autowired
-    public OrderServiceImpl(MemberRepository  memberRepository, DiscountPolicy discountPolicy){
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        System.out.println("discountPolicy = " + discountPolicy);
+        this.discountPolicy = discountPolicy;
+    }
+
+    //생성자가 단 1개 있으면 Autowired 없어도 자동으로 주입해준다.
+    //생성자 주입은 OrderServiceImpl 불러올때 생성자를 불러온다.
+    //new OrderServiceImpl(memberRepository, discountPolicy);
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy){
+        System.out.println("1. OrderServiceImpl.OrderServiceImpl");
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         // discountPolicy 가 알아서 할인 해주고 넘겨줌 만약에 할인율을 바꿀거면 할인만 고치면됨
