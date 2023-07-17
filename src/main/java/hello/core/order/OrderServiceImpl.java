421115c1823e88f2f54f9a3213eaae68e57ab64f
isevou__ <kbry12@naver.com>
2023-07-18 02:12:37 +0900

    add : OrderServiceImplTest 완성

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,23 +12,12 @@ public class OrderServiceImpl implements OrderService{
 
     //불변하는 객체를 바꾸는 Method 조차 만들지 말아야한다.
     //처음부터 불변으로 해놔야지 아니면 나중에 수정하기 힘들어진다.
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
     // 스프링 라이프사이클
     // 1. 스프링 빈 생성
     // 2. 의존관계 주입
-    @Autowired
-    public void setMemberRepository(MemberRepository memberRepository) {
-        System.out.println("memberRepository = " + memberRepository);
-        this.memberRepository = memberRepository;
-    }
-
-    @Autowired
-    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-        System.out.println("discountPolicy = " + discountPolicy);
-        this.discountPolicy = discountPolicy;
-    }
 
     //생성자가 단 1개 있으면 Autowired 없어도 자동으로 주입해준다.
     //생성자 주입은 OrderServiceImpl 불러올때 생성자를 불러온다.
